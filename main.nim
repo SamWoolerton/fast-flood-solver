3b3c335db095d31a4b61a94861a5d5d365337ebd
Sam Woolerton <samuelwoolerton@gmail.com>
2022-04-21 08:00:15 +1200

    Formatting tweaks

--- main.nim
+++ main.nim
@@ -1,4 +1,4 @@
-import std/[strformat,math]
+import std/[strformat, math]
 import sequtils
 from strutils import join
 import sugar
@@ -9,7 +9,7 @@ type CellIndex = int16
 type State = object
   s: seq[Colour]
   sideLength: Natural
-  
+
 type Set = set[Colour]
 
 type
@@ -28,11 +28,13 @@ let startState = block:
   #     5, 2, 3, 3, 2, 5, 2, 3, 3, 2, 3, 5, 6, 4, 3, 3, 5, 4, 6, 6, 2, 5, 2, 6, 3,
   #     2, 6, 6, 3, 6, 5, 5, 3, 3, 4, 5, 5, 2, 1, 6, 6, 3, 6, 3, 3, 5, 6, 3, 2, 6,
   #     4, 3, 2, 4, 4, 6, 3, 5, 4]
-  let state = @[4,3,4,6,3,3,5,5,3,3,4,5,4,4,6,5,6,5,6,5,5,6,5,2,5,3,1,6,4,3,5,5,5,6,1,3,6,3,3,3,5,5,2,3,3,2,5,2,3]
-  # let state = @[4,3,4,6,3,3,5,5,3]
-  # let state = @[4,3,6,4,3,3,5,5,3]
-  # let state = @[4,4,4,4,4,4,5,5,3]
-  
+  let state = @[4, 3, 4, 6, 3, 3, 5, 5, 3, 3, 4, 5, 4, 4, 6, 5, 6, 5, 6, 5, 5,
+      6, 5, 2, 5, 3, 1, 6, 4, 3, 5, 5, 5, 6, 1, 3, 6, 3, 3, 3, 5, 5, 2, 3, 3, 2,
+      5, 2, 3]
+  # let state = @[4, 3, 4, 6, 3, 3, 5, 5, 3]
+  # let state = @[4, 3, 6, 4, 3, 3, 5, 5, 3]
+  # let state = @[4, 4, 4, 4, 4, 4, 5, 5, 3]
+
   State(s: state.map((x) => int16(x)), sideLength: state.len.float.sqrt.round.Natural)
 
 
@@ -48,11 +50,11 @@ proc findCellNeighbours(state: State, index: CellIndex): Set =
   if index <= (state.s.len - l - 1): neighbours.incl((index + l).int16)
   if col > 0: neighbours.incl(index - 1)
   if col < (l - 1): neighbours.incl(index + 1)
-  
+
   return neighbours
 
 proc findAreaNeighbours(state: State, area: Set, newAreaCells: Set, startingAreaNeighbours: Set): Set =
-   # for new area cells, calculate neighbours that aren't in area already
+  # for new area cells, calculate neighbours that aren't in area already
   var newNeighbours: Set = {}
   for n in newAreaCells:
     newNeighbours = newNeighbours + findCellNeighbours(state, n)
@@ -83,7 +85,7 @@ proc findValidMoves(state: State, areaNeighbours: Set): Set =
     options.incl(state.s[cellIndex])
   return options
 
-proc isFinished(areaNeighbours: Set): bool = 
+proc isFinished(areaNeighbours: Set): bool =
   return areaNeighbours.card == 0
 
 proc step(state: State, p: PathState, move: Colour): PathState =
@@ -105,11 +107,11 @@ proc step(state: State, p: PathState, move: Colour): PathState =
 
   return PathState(area: area, areaNeighbours: areaNeighbours, path: newPath)
 
-proc getStartingPathState(state: State): PathState = 
+proc getStartingPathState(state: State): PathState =
   let areaNeighbours: Set = findAreaNeighbours(state, {}, {0.int16}, {0.int16})
   return step(state, PathState(area: {}, areaNeighbours: areaNeighbours, path: Path()), state.s[0])
 
-proc solve(state: State): Path = 
+proc solve(state: State): Path =
   var pathStates: seq[PathState] = @[getStartingPathState(state)]
   var stepCount = 0
   var filledCells: seq[int] = @[]
@@ -131,14 +133,14 @@ proc solve(state: State): Path =
 
         # exit state - return with the first valid path found
         if isFinished(ps.areaNeighbours): return ps.path
-    
+
     pathStates = newPathStates
 
     echo &"Max {filledCells.max} and min {filledCells.min} cells filled"
     filledCells = @[]
 
 
-proc `$`(path: Path): string = 
+proc `$`(path: Path): string =
   var current = path
   # don't print the selection of the starting cell, otherwise it would be a single .previous
   while current.previous.previous != nil:
--- perf.md
+++ perf.md
@@ -1,6 +1,6 @@
 # Timing
 
-Run `time nim -d:release r after.nim`
+Run `time nim -d:release r main.nim`
 
 ## 7x7 with 49 cells, before any performance optimisations:
 
@@ -39,3 +39,33 @@ Run `time nim -d:release r after.nim`
 ## Refactor path state to use much less copying
 
 2m39s
+
+## Fix
+
+Reordered `solve` so progress logs made more sense (max and min calculations are calculated end of the iteration rather than for previous iteration).
+Unexpected performance benefit was that the termination check happened at the start of the loop rather than after each `step` - moving this cut down the runtime dramatically.
+
+42.25s
+
+### Paths
+
+- Starting iteration #1 with 1 paths
+  Max 4 and min 2 cells filled
+- Starting iteration #2 with 2 paths
+  Max 7 and min 4 cells filled
+- Starting iteration #3 with 5 paths
+  Max 15 and min 5 cells filled
+- Starting iteration #4 with 15 paths
+  Max 24 and min 7 cells filled
+- Starting iteration #5 with 50 paths
+  Max 31 and min 8 cells filled
+- Starting iteration #6 with 180 paths
+  Max 37 and min 9 cells filled
+- Starting iteration #7 with 667 paths
+  Max 41 and min 12 cells filled
+- Starting iteration #8 with 2584 paths
+  Max 45 and min 13 cells filled
+- Starting iteration #9 with 10347 paths
+  Max 47 and min 14 cells filled
+- Starting iteration #10 with 40717 paths
+  3 4 5 3 4 5 1 2 3 6
